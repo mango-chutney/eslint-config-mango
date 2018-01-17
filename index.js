3ce15c1c0746caceb0965f7f1efab34ccfa477b4
Ruben Maher <ruben@maher.fyi>
2018-01-17 15:39:03 +1030

    Disable some rules related to prop-types

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ module.exports = {
     node: true
   },
   rules: {
+    'react/default-props-match-prop-types': 0,
     'react/jsx-filename-extension': 0
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
   }
 };
