c27624ef299413beb5574638023b3ac3011bbd5d
Jason Di Benedetto <jason.dibenedetto6@gmail.com>
2018-04-17 12:21:20 +0930

    fix trailing comma bug

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ module.exports = {
   },
   rules: {
     'react/default-props-match-prop-types': 0,
-    'react/jsx-filename-extension': 0
+    'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
-    'react/require-default-props': 0,
+    'react/require-default-props': 0
   }
 };
